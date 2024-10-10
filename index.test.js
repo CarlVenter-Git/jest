587126775d32dd185e3ad2be01a88ec6a2f9e097
Carl Venter <carlventer@live.co.za>
2024-10-10 10:28:10 +0200

    add tests

--- index.test.js
+++ index.test.js
@@ -1,20 +1,27 @@
 const check_list = require('./index');
 
-describe("Index.html Tests", () => {
-    test('true', () => {
-      expect(check_list([3])).toBe('fizz');
-    });
+describe("Additional check_list Tests", () => {
+  test('number divisible by 3', () => {
+      expect(check_list([3, 6, 9])).toBe('fizz, fizz, fizz');
+  });
 
-    test('false', () => {
-      expect(check_list([5])).toBe('buzz');
-    });
+  test('number divisible by 5', () => {
+      expect(check_list([5, 10, 20])).toBe('buzz, buzz, buzz');
+  });
 
-    test('true', () => {
-      expect(check_list([15])).toBe('fizzbuzz');
-    });
+  test('number divisible by both 3 and 5', () => {
+      expect(check_list([15, 30, 45])).toBe('fizzbuzz, fizzbuzz, fizzbuzz');
+  });
 
-    test('true', () => {
-      expect(check_list([3])).toBe('fizz');
-    });
+  test('number not divisible by 3 or 5', () => {
+      expect(check_list([1, 2, 4])).toBe('1, 2, 4');
+  });
 
+  test('mixed numbers', () => {
+      expect(check_list([1, 3, 5, 15])).toBe('1, fizz, buzz, fizzbuzz');
+  });
+
+  test('empty list', () => {
+      expect(check_list([])).toBe('');
+  });
 });
